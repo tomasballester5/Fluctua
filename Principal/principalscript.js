7d35c65019e23ed8bf3989a62552bc4ff90ed954
tomasballester5 <tomasballester5@gmail.com>
2025-10-20 20:44:09 -0300

    Change intro animation to include blur effect

--- Principal/principalscript.js
+++ Principal/principalscript.js
@@ -4,17 +4,17 @@ const main = document.getElementById("main-content");
 const btn = document.getElementById("btn-continuar");
 
 btn.addEventListener("click", () => {
-// Animación de salida suave
+// Animación de salida con blur
+intro.style.transition = "opacity 1s ease, filter 1s ease, transform 1s ease";
 intro.style.opacity = "0";
-intro.style.transform = "translateY(-20px)";
-intro.style.transition = "opacity 0.8s ease, transform 0.8s ease";
-  // Mostrar main después de desvanecer la intro
-setTimeout(() => {
+intro.style.filter = "blur(10px)";
+intro.style.transform = "scale(1.05)";
+  setTimeout(() => {
   intro.style.display = "none";
   main.classList.remove("hidden");
   requestAnimationFrame(() => {
     main.style.opacity = "1";
   });
-}, 800);
+}, 900);
 });
 });
